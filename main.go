697091c4d50c468fd678f2c1bfd2936714937e91
David Guevara <david.guevara4137@alumnos.udg.mx>
2020-09-29 12:12:27 -0500

    euler.

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
-
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main(){
+
+		var (
+			sum = 1.00
+			prod = 1.00
+			i = 1.00
+			dig = 1.00
+		)
+	 
+        
+		fmt.Println("digite el numero: ")
+		fmt.Scan(&dig)
+ 
+        for i=1 ; i <= dig ; i++{
+             prod = prod * i;
+             sum += + (1 / prod);
+         }
+        fmt.Println("El numero e es: ",sum)
+  
 }
